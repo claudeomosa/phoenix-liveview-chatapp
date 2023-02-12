1dfccd835d43f93504e8c690b82bcea0f380c9b3
claudeomosa <claudeomosa@gmail.com>
2023-02-13 00:44:22 +0300

    feat: add button with a phx.click event

--- lib/chat_app_web/live/page/page_live.ex
+++ lib/chat_app_web/live/page/page_live.ex
@@ -1,13 +1,21 @@
 defmodule ChatAppWeb.PageLive do
   use ChatAppWeb, :live_view
+  require Logger
 
+  @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, assign(socket, query: "", result: %{})}
+  end
+
+  @impl true
+  def handle_event("random-room", _params, socket) do
+    Logger.info("click!")
+    {:noreply, socket}
   end
 
   def render(assigns) do
     ~H"""
-      "hello live"
+      <button title="start a room" phx-click="random-room">Start Room</button>
     """
   end
-end
+ end
