be3f0a44601c88a135e40d0595f49627c071666a
claudeomosa <claudeomosa@gmail.com>
2023-02-13 03:20:34 +0300

    build(deps): add uuid

--- mix.exs
+++ mix.exs
@@ -47,7 +47,8 @@ defmodule ChatApp.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:mnemonic_slugs, "~> 0.0.3"},
-      {:tailwind, "~> 0.1", runtime: Mix.env() == :dev}
+      {:tailwind, "~> 0.1", runtime: Mix.env() == :dev},
+      {:uuid, "~> 1.1"}
     ]
   end
 
