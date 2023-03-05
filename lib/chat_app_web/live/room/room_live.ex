d6a2b7f9a345e1bdaf767c33dcb159cb5b3df491
claudeomosa <claudeomosa@gmail.com>
2023-03-05 17:09:43 +0300

    style: chat room

--- lib/chat_app_web/live/room/room_live.ex
+++ lib/chat_app_web/live/room/room_live.ex
@@ -6,21 +6,22 @@ defmodule ChatAppWeb.RoomLive do
   def mount(%{"id" => room_id}, _session, socket) do
     topic = "room" <> room_id
     username = MnemonicSlugs.generate_slug(2)
+
     if connected?(socket) do
       ChatAppWeb.Endpoint.subscribe(topic)
       ChatAppWeb.Presence.track(self(), topic, username, %{})
     end
-   {:ok,
-    assign(socket,
-        room_id: room_id,
-        topic: topic,
-        message: "",
-        messages: [],
-        temporary_assigns: [messages: []],
-        username: username,
-        online_users: []
-      )
-    }
+
+    {:ok,
+     assign(socket,
+       room_id: room_id,
+       topic: topic,
+       message: "",
+       messages: [],
+       temporary_assigns: [messages: []],
+       username: username,
+       online_users: []
+     )}
   end
 
   @impl true
@@ -33,12 +34,12 @@ defmodule ChatAppWeb.RoomLive do
 
   @impl true
   def handle_event("form_update", %{"chat" => %{"message" => message}}, socket) do
-     Logger.info(message: message)
-      {:noreply, assign(socket, message: message)}
+    Logger.info(message: message)
+    {:noreply, assign(socket, message: message)}
   end
+
   @impl true
   def handle_info(%{event: "new-message", payload: message}, socket) do
-
     {:noreply, assign(socket, messages: [message])}
   end
 
@@ -47,16 +48,25 @@ defmodule ChatAppWeb.RoomLive do
     join_messages =
       joins
       |> Map.keys()
-      |> Enum.map(fn username -> %{type: :system, uuid: UUID.uuid4(), text: "#{username} joined the chat"} end)
+      |> Enum.map(fn username ->
+        %{type: :system, uuid: UUID.uuid4(), text: "#{username} joined the chat"}
+      end)
+
     leave_messages =
       leaves
       |> Map.keys()
-      |> Enum.map(fn username -> %{type: :system, uuid: UUID.uuid4(), text: "#{username} left the chart"} end)
+      |> Enum.map(fn username ->
+        %{type: :system, uuid: UUID.uuid4(), text: "#{username} left the chart"}
+      end)
+
     online_users = ChatAppWeb.Presence.list(socket.assigns.topic) |> Map.keys()
 
     Logger.info(online_users)
-    {:noreply, assign(socket, messages: join_messages ++ leave_messages, online_users: online_users)}
+
+    {:noreply,
+     assign(socket, messages: join_messages ++ leave_messages, online_users: online_users)}
   end
+
   def display_message(%{type: :system, uuid: uuid, text: text}) do
     ~E"""
     <p id="<%= uuid%>" class="italic">
@@ -67,20 +77,15 @@ defmodule ChatAppWeb.RoomLive do
 
   def display_message(%{uuid: uuid, text: text, username: username}) do
     ~E"""
-
-    <div class="border-b border-gray-600 py-3 flex items-start mb-4 text-sm" id="<%= uuid%>">
-       <img src="https://cdn.discordapp.com/embed/avatars/4.png" class="cursor-pointer w-10 h-10 rounded-3xl mr-3">
+    <div class="border-b border-gray-600 py-3 flex items-start mb-4 text-sm ml-4" id="<%= uuid%>">
         <div class="flex-1 overflow-hidden">
           <div>
             <span class="font-bold text-red-300 cursor-pointer hover:underline"><%= username%></span>
-            <span class="font-bold text-gray-400 text-xs">12:00</span>
+            <span class="font-bold text-gray-400 text-xs"><%= Time.utc_now.hour %>:<%= Time.utc_now.minute%></span>
           </div>
           <p class="text-white leading-normal"><%= text%></p>
         </div>
      </div>
-
-
     """
-
   end
 end
